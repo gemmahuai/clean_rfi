6927d138dd5c3ce52f928cb45d99dd504510b66a
Kiran Shila <me@kiranshila.com>
2024-04-08 16:06:57 -0700

    Make python build an optional feature to not bloat the CLI

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,29 @@ pub mod algos;
 pub mod io;
 pub mod math;
 
-use faer_ext::IntoFaer;
-use numpy::{ndarray::ArrayViewMut2, PyArray2, PyArrayMethods};
-use pyo3::prelude::*;
+#[cfg(feature = "python")]
+pub mod python {
+    use super::*;
 
-#[pymodule]
-fn clean_rfi<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
-    fn clean_block(mat: ArrayViewMut2<'_, f32>) {
-        // Transpose so the Mat is column-major when we hand it to faer
-        let faer_block = mat.into_faer().transpose_mut();
-        algos::clean_block(faer_block);
+    use faer_ext::IntoFaer;
+    use numpy::{ndarray::ArrayViewMut2, PyArray2, PyArrayMethods};
+    use pyo3::prelude::*;
+
+    #[pymodule]
+    fn clean_rfi<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
+        fn clean_block(mat: ArrayViewMut2<'_, f32>) {
+            // Transpose so the Mat is column-major when we hand it to faer
+            let faer_block = mat.into_faer().transpose_mut();
+            algos::clean_block(faer_block);
+        }
+
+        #[pyfn(m)]
+        #[pyo3(name = "clean_block")]
+        fn clean_block_py(mat: &Bound<'_, PyArray2<f32>>) {
+            let mat = unsafe { mat.as_array_mut() };
+            clean_block(mat);
+        }
+
+        Ok(())
     }
-
-    #[pyfn(m)]
-    #[pyo3(name = "clean_block")]
-    fn clean_block_py(mat: &Bound<'_, PyArray2<f32>>) {
-        let mat = unsafe { mat.as_array_mut() };
-        clean_block(mat);
-    }
-
-    Ok(())
 }
